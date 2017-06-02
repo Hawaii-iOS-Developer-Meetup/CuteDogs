60b92ab42e791690c1e213d384a250e983d372ab
davidkneely <deeniri@gmail.com>
2017-06-01 18:35:22 -1000

    Initial Commit

---
+++ CuteDogs/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CuteDogs
+//
+//  Created by pair on 6/1/17.
+//  Copyright © 2017 pair. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
